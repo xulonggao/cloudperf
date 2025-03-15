f610085a28ab23c9a24b1f6a69bfbc0e7fbb18e7
Lijia, Tang <barrytan@amazon.com>
2025-03-15 19:02:32 +0800

    bug fix

--- script/upload_sql.sh
+++ script/upload_sql.sh
@@ -9,4 +9,4 @@ fi
 
 S3_BUCKET=$(aws cloudformation describe-stacks --stack-name CloudperfStack --query 'Stacks[0].Outputs[?OutputKey==`s3Bucket`].OutputValue' --output text --region us-east-1)
 FILENAME=$(basename "${sqlfile}")
-aws s3 cp s3://${S3_BUCKET}/import-sql/${FILENAME} ${sqlfile}
+aws s3 cp ${sqlfile} s3://${S3_BUCKET}/import-sql/${FILENAME}
